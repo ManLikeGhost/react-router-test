a859f04bfe87c0c2feda8b3f00ce69aa745287b1
ManLikeGhost <ekpoabasiama@gmail.com>
2020-07-20 16:52:58 +0100

    router test

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Home from './Home';
+import About from './About';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+ReactDOM.render(<Router>
+  <div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/About">About-me</Link></li>
+    </ul>
+
+    <Route exact path="/" component={Home} />
+    <Route path="/About" component={About} />
+  </div>
+</Router>
+  , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
